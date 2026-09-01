f89130f84293000b3e4556ffbe2696a87749c0eb
zizitop13 <zinmv@outlook.com>
2026-09-01 15:20:57 +0200

    Add src/test/kotlin/io/github/zizitop13/redhotcode/PluginSmokeTest.kt

---
+++ src/test/kotlin/io/github/zizitop13/redhotcode/PluginSmokeTest.kt
@@ -0,0 +1,11 @@
+package io.github.zizitop13.redhotcode
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PluginSmokeTest {
+    @Test
+    fun pluginProjectIsConfigured() {
+        assertEquals("red-hot-code", "red-hot-code")
+    }
+}
